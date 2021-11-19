088958ea2fdd5c384bbef755c5f0b8e94bf5feaa
Padmashree14santhosh <92292526+Padmashree14santhosh@users.noreply.github.com>
2021-11-19 21:56:50 +0530

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,27 @@
+// a. typeof(1)
+let a=typeof(1);
+console.log(a);
+// b.	typeof(1.1)
+let b=typeof(1.1);
+console.log(b);
+// // c.	typeof('100.234')
+let c=typeof('100.234')
+console.log(c);
+// // d.	typeof(true)
+let d=typeof(true)
+console.log(d);
+// // e.	typeof(null)
+let e=typeof(null)
+console.log(e);
+// // f.	typeof(undefined)
+let f=typeof(undefined)
+console.log(f);
+// // g.	typeof([])
+let g=typeof([]);
+console.log(g);
+// // h.	typeof({})
+let h=typeof({})
+console.log(h);
+// // i.	typeof(NaN)
+let i=typeof(NaN)
+console.log(i);
